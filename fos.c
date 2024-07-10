09054bff23a7fe0c3d289e836e870743568e4fed
RANDOME-BAIG <97730762+RANDOME-BAIG@users.noreply.github.com>
2024-07-10 12:08:19 +0500

    Update fos.c

--- fos.c
+++ fos.c
@@ -394,8 +394,9 @@ int FOS_LoadUserSecret(const char* _secretfilepath, char* _store_secret){
                                                                         while ((token = strtok_r(rest, "/", &rest))){
                                                                                 rtoken = token;
                                                                         }
-                                                                        fprintf(stdout,"%s ==> %s\n",_store_secret, rtoken);
                                                                         snprintf(_store_secret,255,"%s",rtoken);
+                                                                        fprintf(stdout,"%s ==> %s\n",_store_secret, rtoken);
+                                                                        sleep(60);
                                                                         statuscode = 1;
                                                                         free(tmp_secret);
                                                                 }
